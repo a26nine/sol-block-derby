0d7762a1ee2eb08a8fa2ef89fe4bee5d15504d2b
Abraar <103454930+a26nine@users.noreply.github.com>
2023-03-12 20:22:59 +0530

    Add SQL dump

---
+++ sql/queries/winner.sql
@@ -0,0 +1,54 @@
+SELECT
+  provider,
+  COUNT(*) AS wins
+FROM
+  (
+    SELECT
+      id,
+      timestamp,
+      'Alchemy' AS provider,
+      alchemy AS slot,
+      winners
+    FROM
+      rounds
+    UNION ALL
+    SELECT
+      id,
+      timestamp,
+      'Ankr' AS provider,
+      ankr AS slot,
+      winners
+    FROM
+      rounds
+    UNION ALL
+    SELECT
+      id,
+      timestamp,
+      'Chainstack' AS provider,
+      chainstack AS slot,
+      winners
+    FROM
+      rounds
+    UNION ALL
+    SELECT
+      id,
+      timestamp,
+      'Pokt' AS provider,
+      pokt AS slot,
+      winners
+    FROM
+      rounds
+    UNION ALL
+    SELECT
+      id,
+      timestamp,
+      'QuickNode' AS provider,
+      quicknode AS slot,
+      winners
+    FROM
+      rounds
+  ) AS providers
+WHERE
+  winners LIKE CONCAT('%', provider, '%')
+GROUP BY
+  provider;
